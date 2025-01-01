811a41481d1b757eeb4ab6c3c2641a731f23bfc9
tchisama <pro.tchisama@gmail.com>
2025-01-01 16:54:42 +0100

    good

--- src/components/calendly.tsx
+++ src/components/calendly.tsx
@@ -4,13 +4,13 @@ import { InlineWidget } from 'react-calendly';
 
 const Calendly = () => {
     return (
-        <div className="h-fit py-20">
+        <div className="h-fit  pt-4">
             <InlineWidget
             styles={{
-              overflow: 'hidden',
-              height: '70vh',
-            }}
-             url="https://calendly.com/pro-tchisama?hide_landing_page_details=1&hide_gdpr_banner=1&text_color=222222&primary_color=222222" />
+                height: '700px',
+                width: '100%',
+            }}  
+              url="https://calendly.com/pro-tchisama?hide_landing_page_details=1&hide_gdpr_banner=1&text_color=222222&primary_color=222222" />
         </div>
     );
 };
